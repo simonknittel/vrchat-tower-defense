12f3887082ef7cffde43d97442459c467b008592
Simon Knittel <simon.knittel@hmmh.de>
2022-12-31 14:50:20 +0100

    feat: Implement basic tower spawning

---
+++ unity/Assets/GameManager.cs
@@ -0,0 +1,75 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace SimonKnittel.TowerDefense
+{
+	[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+	public class GameManager : UdonSharpBehaviour
+	{
+		public int Gold = 1000;
+		public int SingleTargetDamageCosts = 100;
+
+		VRCPlayerApi _localPlayer;
+		TowerTile.Manager _currentHighlightedTowerTile;
+		bool _isUserInVR = true;
+		TowerTile.TowerTypes _currentInventorySelection = TowerTile.TowerTypes.SingleTargetDamage;
+
+		void Start()
+		{
+			_localPlayer = Networking.LocalPlayer;
+			_isUserInVR = _localPlayer.IsUserInVR();
+		}
+
+		void Update()
+		{
+			if (_currentHighlightedTowerTile != null)
+			{
+				_currentHighlightedTowerTile.ToggleHighlight(false);
+				_currentHighlightedTowerTile = null;
+			}
+
+			VRCPlayerApi.TrackingData trackingData;
+			if (_isUserInVR)
+			{
+				trackingData = _localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.RightHand);
+			}
+			else
+			{
+				trackingData = _localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+			}
+			var origin = trackingData.position;
+			var direction = trackingData.rotation * Vector3.forward;
+			var ray = new Ray(origin, direction);
+
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 5f))
+			{
+				if (hit.transform && hit.transform.gameObject.name.Contains("RaycastTarget"))
+				{
+					_currentHighlightedTowerTile = hit.transform.gameObject.GetComponentInParent<TowerTile.Manager>();
+					_currentHighlightedTowerTile.ToggleHighlight(true);
+				}
+			}
+		}
+
+		public void InputUse()
+		{
+			if (_currentHighlightedTowerTile == null) return;
+
+			switch (_currentInventorySelection)
+			{
+				case TowerTile.TowerTypes.SingleTargetDamage:
+					if (Gold < SingleTargetDamageCosts) return;
+					if (_currentHighlightedTowerTile.SpawnTower(TowerTile.TowerTypes.SingleTargetDamage) == false) return;
+					Gold -= SingleTargetDamageCosts;
+					return;
+
+				default:
+					return;
+			}
+		}
+	}
+}
---
+++ unity/Assets/Tiles/TowerTile/Manager.cs
@@ -0,0 +1,43 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace SimonKnittel.TowerDefense.TowerTile
+{
+	public enum TowerTypes
+	{
+		None,
+		SingleTargetDamage,
+	}
+
+	[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+	public class Manager : UdonSharpBehaviour
+	{
+		public MeshRenderer RaycastTargetRenderer;
+		public GameObject SingleTargetDamage;
+		TowerTypes _activeTower = TowerTypes.None;
+
+		public void ToggleHighlight(bool newValue)
+		{
+			RaycastTargetRenderer.enabled = newValue;
+		}
+
+		public bool SpawnTower(TowerTypes type)
+		{
+			if (_activeTower != TowerTypes.None) return false;
+
+			switch (type)
+			{
+				case TowerTypes.SingleTargetDamage:
+					SingleTargetDamage.SetActive(true);
+					_activeTower = type;
+					return true;
+
+				default:
+					return false;
+			}
+		}
+	}
+}
